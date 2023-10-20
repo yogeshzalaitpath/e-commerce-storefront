c9dab469f0f1512c1e0a79a6ccb5413dabebe6fe
Yogesh Zala <yogeshz.itpath@gmail.com>
2023-10-20 11:49:32 +0530

    Updated next configs

--- next.config.js
+++ next.config.js
@@ -14,8 +14,9 @@ module.exports = withStoreConfig({
   images: {
     domains: [
       "medusa-public-images.s3.eu-west-1.amazonaws.com",
-      "localhost",
       "medusa-server-testing.s3.amazonaws.com",
+      "res.cloudinary.com",
+      "localhost"
     ],
   },
 })
